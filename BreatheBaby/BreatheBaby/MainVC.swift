bc667d0010b7d3ee93ecab663db73f2d08a15e4c
rockgarden <ro>
2017-05-22 16:14:15 +0800

    add

---
+++ BreatheBaby/BreatheBaby/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  BreatheBaby
+//
+//  Created by wangkan on 2017/5/22.
+//  Copyright © 2017年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var testNNA: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ BreatheBaby/NeuralNet/NeuralNet.h
@@ -0,0 +1,19 @@
+//
+//  NeuralNet.h
+//  NeuralNet
+//
+//  Created by wangkan on 2017/5/22.
+//  Copyright © 2017年 rockgarden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeuralNet.
+FOUNDATION_EXPORT double NeuralNetVersionNumber;
+
+//! Project version string for NeuralNet.
+FOUNDATION_EXPORT const unsigned char NeuralNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeuralNet/PublicHeader.h>
+
+
